0c53bc5c46d49d59da601cffab137b51c7a096b0
Robert Biały <bialyrobert12@gmail.com>
2020-12-09 21:52:46 +0100

    Added jwt support

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import GoogleTrendsController from '../controllers/google-trends-controller';
+import AuthenticationController from '../controllers/authentication-controller'
 const routes = Router();
 
-routes.get('/dailyTrends', (request, response) => GoogleTrendsController.getDailyTrends(request,  response));
-routes.get('/interest', (request, response) => GoogleTrendsController.interestOverTime(request,  response));
-routes.get('/realTimeTreands', (request, response) => GoogleTrendsController.realTimeTrends(request,  response));
+routes.get('/dailyTrends', (req, res, next) => AuthenticationController.verifyToken(req, res, next), (request, response) => GoogleTrendsController.getDailyTrends(request,  response));
+routes.get('/interest', (req, res, next) => AuthenticationController.verifyToken(req, res, next), (request, response) => GoogleTrendsController.interestOverTime(request,  response));
+routes.get('/realTimeTreands', (req, res, next) => AuthenticationController.verifyToken(req, res, next), (request, response) => GoogleTrendsController.realTimeTrends(request,  response));
 
-routes.get('/', function (req, res) {
-    res.send('hello world')
-})
+routes.post('/login', (request, response) => AuthenticationController.login(request, response));
+routes.get('/', (req, res) => res.send('hello world'));
 
 export default routes;
